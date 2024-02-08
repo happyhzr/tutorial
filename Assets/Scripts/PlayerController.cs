cb16dc5cf928f2244da984c398856b6f96a52778
happyhzr <insisthzr@163.com>
2024-02-08 20:49:28 +0800

    Interfaces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -62,7 +62,7 @@ public class PlayerController : MonoBehaviour
         var collider = Physics2D.OverlapCircle(interactPos, 0.2f, iteractableLayer);
         if (collider != null)
         {
-            Debug.Log("There is an NPC here!");
+            collider.GetComponent<Interactable>()?.Interact();
         }
     }
 
